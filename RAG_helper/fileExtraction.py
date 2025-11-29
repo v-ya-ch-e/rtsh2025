3b751d9f5009294fe6920eac62cf24adf6ae4ce2
Family Games <jalevant239@gmail.com>
2025-11-30 00:00:55 +0100

    RAG_commit#3

--- RAG_helper/fileExtraction.py
+++ RAG_helper/fileExtraction.py
@@ -4,13 +4,13 @@ from llama_index.core import SimpleDirectoryReader
 import chromadb
 import constants
 # 1. Setup Persistent Storage
-db_path = "/Users/zero_skill/Documents/negotiation_DB"
+db_path = "/Users/zero_skill/Documents/context_company_1"
 db = chromadb.PersistentClient(path=db_path)
 chroma_collection = db.create_collection(
-    name="test_collection_1",
+    name="collection_dev_01",
     embedding_function=OpenAIEmbeddingFunction(
         api_key=open("cred").read(),
-        model_name="text-embedding-3-large"
+        model_name="text-embedding-3-small"
     )
 )
 #vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
@@ -21,16 +21,15 @@ chroma_collection = db.create_collection(
 # It uses file extensions to decide how to parse.
 print("Loading files... this may take a moment but happens only once.")
 reader = SimpleDirectoryReader("/Users/zero_skill/Documents/negotiation_info")
-llama_documents = reader.load_data()
+documents = reader.load_data()
+llama_documents = list((documents[0].text).split("\n## "))
+#print(llama_documents[1])
 docs_text = []
 docs_ids = []
 for i, doc in enumerate(llama_documents):
     # Extract the raw text
-    docs_text.append(doc.text)
+    docs_text.append(doc)
 
-    # Create a unique ID (id+Index)
-    # LlamaIndex docs have an internal ID, but making a readable one is often better
-    # filename = doc.metadata.get('file_name', 'unknown_file')
     unique_id = f"id_{i}"
     docs_ids.append(unique_id)
 
--- RAG_helper/processQuery.py
+++ RAG_helper/processQuery.py
@@ -5,25 +5,27 @@ from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
 from RAG_helper.constants import QUERY_RES_LIMIT
 
 # 2. CONNECT TO DATABASE (No ingestion logic here)
-db_path = "/Users/zero_skill/Documents/negotiation_DB"
-db = chromadb.PersistentClient(path=db_path)
-chroma_collection = db.get_collection(
-    name="test_collection_1",
-    embedding_function=OpenAIEmbeddingFunction(
-        api_key=open("cred").read(),
-        model_name="text-embedding-3-large"
+def processQuery(query, company_id):
+    db_path = "/Users/zero_skill/Documents/context_company_" + str(company_id)
+    db = chromadb.PersistentClient(path=db_path)
+    chroma_collection = db.get_collection(
+        name="collection_dev_0" + str(company_id),
+        embedding_function=OpenAIEmbeddingFunction(
+            api_key=open("cred").read(),
+            model_name="text-embedding-3-small"
+        )
     )
-)
 
-print("Collection loaded. Running query...")
+    # print("Collection loaded. Running query...")
 
-query_text = "Find all prices mentioned in the files"
-results = chroma_collection.query(
-    query_texts=[query_text],
-    n_results=QUERY_RES_LIMIT
-)
+    results = chroma_collection.query(
+        query_texts=[query],
+        n_results=QUERY_RES_LIMIT
+    )
 
-# 5. DISPLAY RESULTS
-print(f"Found {len(results)} relevant snippets.")
-context_str = "\n".join([n for n in results["documents"][0]])
-print(context_str)
+    # 5. DISPLAY RESULTS
+    # print(f"Found {len(results)} relevant snippets.")
+    context_str = "\n".join([n for n in results["documents"][0]])
+    return context_str
+
+print(processQuery("product prices", 1))

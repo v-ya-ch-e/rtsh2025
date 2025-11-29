3e458fc327b7251c12ed9f2cde2165d0169b1929
Family Games <jalevant239@gmail.com>
2025-11-29 20:56:20 +0100

    RAG_commit#2

--- RAG_helper/fileExtraction.py
+++ RAG_helper/fileExtraction.py
@@ -1,28 +1,42 @@
 # install: pip install llama-index llama-index-vector-stores-chroma chromadb llama-parse
-import os
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext
-from llama_index.vector_stores.chroma import ChromaVectorStore
-from llama_index.embeddings.openai import OpenAIEmbedding
-from llama_index.core import VectorStoreIndex
-from llama_index.core import Settings
+from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
+from llama_index.core import SimpleDirectoryReader
 import chromadb
-Settings.embed_model = OpenAIEmbedding(api_key=open("cred").read())
-# 1. Setup Persistent Storage (Saves to folder "./chroma_db")
-db = chromadb.PersistentClient(path="/Users/zero_skill/Documents/negotiation_DB")
-chroma_collection = db.get_or_create_collection("my_docs")
-vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-storage_context = StorageContext.from_defaults(vector_store=vector_store)
+import constants
+# 1. Setup Persistent Storage
+db_path = "/Users/zero_skill/Documents/negotiation_DB"
+db = chromadb.PersistentClient(path=db_path)
+chroma_collection = db.create_collection(
+    name="test_collection_1",
+    embedding_function=OpenAIEmbeddingFunction(
+        api_key=open("cred").read(),
+        model_name="text-embedding-3-large"
+    )
+)
+#vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+#storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
 # 2. High-Quality Loading
 # SimpleDirectoryReader automatically handles PDF, JSON, DOCX, TXT.
 # It uses file extensions to decide how to parse.
 print("Loading files... this may take a moment but happens only once.")
-documents = SimpleDirectoryReader("/Users/zero_skill/Documents/negotiation_info").load_data()
+reader = SimpleDirectoryReader("/Users/zero_skill/Documents/negotiation_info")
+llama_documents = reader.load_data()
+docs_text = []
+docs_ids = []
+for i, doc in enumerate(llama_documents):
+    # Extract the raw text
+    docs_text.append(doc.text)
 
-# 3. Build & Save Index
-index = VectorStoreIndex.from_documents(
-    documents,
-    storage_context=storage_context
+    # Create a unique ID (id+Index)
+    # LlamaIndex docs have an internal ID, but making a readable one is often better
+    # filename = doc.metadata.get('file_name', 'unknown_file')
+    unique_id = f"id_{i}"
+    docs_ids.append(unique_id)
+
+chroma_collection.add(
+    ids=docs_ids,
+    documents=docs_text
 )
 
 print("✅ Index saved to disk! You can now run queries instantly.")
